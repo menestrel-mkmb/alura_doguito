8489a70358aad36cddbd9311acde57df9273efa2
Michael Kevin <michaelkmb@hotmail.com>
2022-04-10 09:21:02 -0300

    span statusError Doguito;

--- js/validacao.js
+++ js/validacao.js
@@ -1,16 +1,59 @@
 const dataNascimento = document.querySelector('#nascimento');
 
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
+const statusErrorMsg = {
+    nome: {
+        valueMissing: `O campo nome não pode estar vazio`,
+    },
+    email: {
+        valueMissing: `O campo e-mail não pode estar vazio`,
+        typeMismatch: `O e-mail deve ter um formato válido`,
+    },
+    senha: {
+        valueMissing: `O campo senha não pode estar vazio`,
+        patternMismatch: `A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e não pode conter símbolos`,
+    },
+    dataNascimento: {
+        valueMissing: `O campo data de nascimento não pode estar vazio`,
+        customError: `Você deve ser maior de idade para se cadastrar`,
+    }
+
+}
+
+const typeErrorMsg = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
 export function valida(input){
     const tipoInput = input.dataset.tipo;
     if(validadores[tipoInput]){
         validadores[tipoInput](input);
     }
 
-    (input.validity.valid) ? input.parentElement.classList.remove('input-container--invalido') : input.parentElement.classList.add('input-container--invalido');
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = returnErrorMessage(tipoInput, input);
+    }
 }
 
-const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+function returnErrorMessage(tipoInput, input){
+    let mensagem = '';
+
+    typeErrorMsg.forEach(erro => {
+        if(input.validity[erro])
+            mensagem = statusErrorMsg[tipoInput][erro];
+    })
+
+    return mensagem;
 }
 
 dataNascimento.addEventListener('blur',(event) => {
