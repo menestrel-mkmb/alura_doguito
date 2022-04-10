fc3cdcce45824bc8a336f1118f9232540d3389e7
Michael Kevin <michaelkmb@hotmail.com>
2022-04-10 09:02:30 -0300

    parser input validity Doguito;

--- js/validacao.js
+++ js/validacao.js
@@ -1,18 +1,30 @@
 const dataNascimento = document.querySelector('#nascimento');
 
+export function valida(input){
+    const tipoInput = input.dataset.tipo;
+    if(validadores[tipoInput]){
+        validadores[tipoInput](input);
+    }
+
+    (input.validity.valid) ? input.parentElement.classList.remove('input-container--invalido') : input.parentElement.classList.add('input-container--invalido');
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
 dataNascimento.addEventListener('blur',(event) => {
     validaDataNascimento(event.target);
 });
 
 function validaDataNascimento(data){
-    // if( !(data.value == 'string') )
+    // if( !(typeof(data.value) == 'string') )
     //     return;
     
     let mensagem = '';
 
     const dataRecebida = new Date(data.value);
 
-    console.log(maiorIdade(dataRecebida, 18));
     if (!maiorIdade(dataRecebida, 18)) {
         mensagem = 'Você precisa ser maior de idade';
     }
@@ -20,7 +32,7 @@ function validaDataNascimento(data){
 }
 
 function maiorIdade(data, threshold){
-    // if( !(data == 'string') || !(threshold == 'number') )
+    // if( !(typeof(data) == 'string') || !(typeof(threshold) == 'number') )
     //     return;
     const dataAtual = new Date();
     const dataTeste = new Date(data.getUTCFullYear()+threshold, data.getUTCMonth(), data.getUTCDay());
